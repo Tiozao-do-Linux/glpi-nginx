a875f2e7caa7fc1f3abbc01d2c97dc5f85d4eb45
Jarbas Peixoto Júnior <jarbas.junior@gmail.com>
2025-11-13 18:35:19 -0400

    Disable GLPI installation checks in entrypoint.sh
    Comment out GLPI installation and requirement checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,40 +34,40 @@ echo_line "PHP Loaded Modules"
 FORMATTED=$(php -m | awk '/^\[/{if(NR>1)print prev; prev=$0; next}{gsub(/^\s+|\s+$/,""); if($0) prev=prev" "$0} END{print prev}')
 echo "$FORMATTED"
 
-echo_line "Check system requirements to run GLPI"
-if php glpi/bin/console system:check_requirements
-then
-    echo_line "Check if GLPI is already installed"
-    if php glpi/bin/console database:check_schema_integrity
-    then
-        echo_line "GLPI is already installed"
-    else
-        echo_line "Install GLPI Automatically is disabled by default."
-        echo_line "You can enable it by setting the GLPI_INSTALL environment variable to true."
+# echo_line "Check system requirements to run GLPI"
+# if php glpi/bin/console system:check_requirements
+# then
+#     echo_line "Check if GLPI is already installed"
+#     if php glpi/bin/console database:check_schema_integrity
+#     then
+#         echo_line "GLPI is already installed"
+#     else
+#         echo_line "Install GLPI Automatically is disabled by default."
+#         echo_line "You can enable it by setting the GLPI_INSTALL environment variable to true."
 
-        # echo_line "Is Update?"
-        # php glpi/bin/console database:update
+#         # echo_line "Is Update?"
+#         # php glpi/bin/console database:update
 
-        # echo_line "Enable maintenance mode"
-        # php glpi/bin/console maintenance:enable
+#         # echo_line "Enable maintenance mode"
+#         # php glpi/bin/console maintenance:enable
 
-        # php glpi/bin/console db:install \
-        # --default-language="$GLPI_LANG" \
-        # --db-host="$GLPI_DB_HOST" \
-        # --db-port="$GLPI_DB_PORT" \
-        # --db-name="$GLPI_DB_NAME" \
-        # --db-user="$GLPI_DB_USER" \
-        # --db-password="$GLPI_DB_PASSWORD" \
-        # --no-interaction \
-        # --reconfigure
+#         # php glpi/bin/console db:install \
+#         # --default-language="$GLPI_LANG" \
+#         # --db-host="$GLPI_DB_HOST" \
+#         # --db-port="$GLPI_DB_PORT" \
+#         # --db-name="$GLPI_DB_NAME" \
+#         # --db-user="$GLPI_DB_USER" \
+#         # --db-password="$GLPI_DB_PASSWORD" \
+#         # --no-interaction \
+#         # --reconfigure
 
-        # echo_line "Disable maintenance mode"
-        # php glpi/bin/console maintenance:disable
-    fi
-else
-    echo_line "Requirements not met!";
-    exit 1
-fi
+#         # echo_line "Disable maintenance mode"
+#         # php glpi/bin/console maintenance:disable
+#     fi
+# else
+#     echo_line "Requirements not met!";
+#     exit 1
+# fi
 
 
 _DATE_TIME=`date`
